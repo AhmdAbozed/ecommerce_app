df2d62bdc5923a4ef8f819816b6686df32cf97cf
ahmd hassan abozed <ahmdabozed88@gmail.com>
2022-08-26 19:21:44 +0200

    Fully implemented sign in and sign up pages

---
+++ src/util/tokenauth.ts
@@ -0,0 +1,38 @@
+import express from "express"
+import { Request, Response } from 'express'
+import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
+
+dotenv.config()
+
+const {tokenSecret} = process.env;
+
+const verifyAuthToken = (req: Request, res: Response, next: () => void) => {
+    try {
+      req.cookies.token
+        if (req.cookies.token) {
+          const token: string = req.cookies.token;
+          const decoded = jwt.verify(token, tokenSecret as string)
+        next()
+      } else throw("error");
+    } catch (error) {
+      res.status(200).send("JWT auth failed.")
+    }
+  }
+
+  const redirectToHome = (req: Request, res: Response, next: () => void) => {
+    try {
+        if (req.cookies.token) {
+        const token: string = req.cookies.token;
+        const decoded = jwt.verify(token, tokenSecret as string)
+        res.redirect("/user")
+      } else throw("error");
+    } catch (error) {
+      next()
+    }
+  }
+
+  export {
+    verifyAuthToken,
+    redirectToHome
+  }
